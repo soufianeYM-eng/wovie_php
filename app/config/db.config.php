a9780ec53a1ea11dd48b6a1f6effe883116d2657
Soufiane <yousfisoufiane8@gmail.com>
2022-01-15 03:23:01 +0100

    Fix gitignore

--- app/config/db.config.php
+++ app/config/db.config.php
@@ -3,7 +3,7 @@
  * Define database config
  */ 
 
-define("DB_HOST","localhost:3306"); 
-define("DB_NAME","wovie_db"); 
-define("DB_USER","root"); 
-define("DB_PASS","root");  
+define("DB_HOST","eu-cdbr-west-02.cleardb.net:3306"); 
+define("DB_NAME","heroku_65ca1a64ba3cb7b"); 
+define("DB_USER","bacda30ff1a7ac"); 
+define("DB_PASS","69710404"); 
