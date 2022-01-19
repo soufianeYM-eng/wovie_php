096b909974cb0aaa23fa609fb02542bc3cf3a94d
Soufiane <yousfisoufiane8@gmail.com>
2022-01-19 18:15:45 +0100

    Fix payment error

--- app/config/db.config.php
+++ app/config/db.config.php
@@ -7,10 +7,11 @@
 // define("DB_USER","bacda30ff1a7ac"); 
 // define("DB_PASS","69710404"); 
 
-define("DB_HOST","W_HOST"); 
-define("DB_NAME","W_NAME"); 
-define("DB_USER","W_USER"); 
-define("DB_PASS","W_PASS");  
+//prod
+define("DB_HOST","localhost"); 
+define("DB_NAME","aeonsrms_aeonnovel_db"); 
+define("DB_USER","aeonsrms_aennovel"); 
+define("DB_PASS","aennovel2022");   
 
 // define("DB_HOST","localhost:3306"); 
 // define("DB_NAME","wovie_mdb"); 
